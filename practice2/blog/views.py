191232a9019c6ac314c649b35f1d0646d6b319a3
Grizzled Squirrel <grizzledsquirrel@gmail.com>
2016-06-15 14:09:24 +0300

    Changed the HTML for the site.

--- practice2/blog/views.py
+++ practice2/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+	return render(request, 'blog/post_list.html',{})
+
+def irshadlanding(request):
+	return render(request, 'blog/stud-home.html',{})
